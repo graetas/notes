45c8349a1eda954206287eef10136f0a7754ac05
Greta Ritchard <gretaritchard@Gretas-MBP-2.modem>
2024-03-10 20:02:28 +1100

    Quartz sync: Mar 10, 2024, 8:02 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -16,11 +16,11 @@ export const sharedPageComponents: SharedLayout = {
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
+    Component.MobileOnly(Component.Explorer()),
     Component.Breadcrumbs(),
     Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
-    Component.MobileOnly(Component.Explorer()),
   ],
   left: [
     Component.PageTitle(),
